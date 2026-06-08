89e5fa2724a0d68cd154dc3bfeaf294d538c77b0
Gabriel Magnus <gmmb@cesar.school>
2026-06-08 16:20:10 -0300

    Atualizar alertas.py com função de cadastro de animal e tarefas

--- Projeto---Unidade-2-Grupo-8/principal/alertas.py
+++ Projeto---Unidade-2-Grupo-8/principal/alertas.py
@@ -1,41 +1,96 @@
-﻿from datetime import datetime
+from datetime import datetime
 
 
 def calcular_dias_restantes(data_tarefa):
     try:
         hoje = datetime.today().date()
-        data_convertida = datetime.strptime(data_tarefa, "%d/%m/%Y").date()
+        data_convertida = datetime.strptime(
+            data_tarefa,
+            "%d/%m/%Y"
+        ).date()
+
         return (data_convertida - hoje).days
+
     except ValueError:
         return None
 
 
+def cadastrar_animal():
+    nome = input(
+        "Digite o nome do animal: "
+    )
+
+    tarefas = []
+
+    quantidade = int(
+        input(
+            "Quantas tarefas o animal possui? "
+        )
+    )
+
+    for i in range(quantidade):
+        print(f"\nTarefa {i + 1}")
+
+        tipo = input(
+            "Digite o nome da tarefa: "
+        )
+
+        data = input(
+            "Digite a data da tarefa "
+            "(DD/MM/AAAA): "
+        )
+
+        tarefa = {
+            "tipo": tipo,
+            "data": data
+        }
+
+        tarefas.append(tarefa)
+
+    animal = {
+        "nome": nome,
+        "tarefas": tarefas
+    }
+
+    return animal
+
+
 def exibir_alertas(animal):
-    print(f"\n=== ALERTAS DE {animal['nome'].upper()} ===")
+    print(
+        f"\n=== ALERTAS DE "
+        f"{animal['nome'].upper()} ==="
+    )
 
     for tarefa in animal["tarefas"]:
-        dias = calcular_dias_restantes(tarefa["data"])
+        dias = calcular_dias_restantes(
+            tarefa["data"]
+        )
 
         if dias is None:
-            print(f"{tarefa['tipo']}: data inválida")
+            print(
+                f"{tarefa['tipo']}: "
+                f"data inválida"
+            )
 
         elif dias > 0:
-            print(f"{tarefa['tipo']}: faltam {dias} dias")
+            print(
+                f"{tarefa['tipo']}: "
+                f"faltam {dias} dias"
+            )
 
         elif dias == 0:
-            print(f"{tarefa['tipo']}: é hoje")
+            print(
+                f"{tarefa['tipo']}: "
+                f"é hoje"
+            )
 
         else:
-            print(f"{tarefa['tipo']}: atrasado há {abs(dias)} dias")
+            print(
+                f"{tarefa['tipo']}: "
+                f"atrasado há "
+                f"{abs(dias)} dias"
+            )
 
 
-animal = {
-    "nome": "Rex",
-    "tarefas": [
-        {"tipo": "Vacina V10", "data": "20/06/2026"},
-        {"tipo": "Consulta veterinária", "data": "08/06/2026"},
-        {"tipo": "Banho", "data": "01/06/2026"}
-    ]
-}
-
+animal = cadastrar_animal()
 exibir_alertas(animal)
